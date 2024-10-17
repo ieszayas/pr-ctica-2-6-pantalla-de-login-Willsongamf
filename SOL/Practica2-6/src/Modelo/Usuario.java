b932d8c9dd715f34ce01308934483fd8fe999b69
Rodrigo Calvo Mèrida <160269872+Willsongamf@users.noreply.github.com>
2024-10-17 17:05:21 +0200

    Commit_HaciaCasa

---
+++ SOL/Practica2-6/src/Modelo/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author DAM2_09
+ */
+public class Usuario {
+    private String nombre = "";
+    private String contraseña = "";
+
+    public Usuario(String nombre, String contraseña) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+        
+        
+    }
+ 
+    
+    
+}
